7e580c13acd19414e648ec54145a098947fd8e27
ndsorrowchi <ndsorrowchi@gmail.com>
2015-04-11 23:29:55 -0400

    changed ip

--- javascripts/knockout_models/global.js
+++ javascripts/knockout_models/global.js
@@ -1,4 +1,4 @@
 var isInDebug = false;
 
-var restBaseUrl = "http://127.0.0.1:7654/";
+var restBaseUrl = "http://54.174.80.167:7654/";
 var metaStoreGetDataSourcesURL =  "http://54.152.26.131:7654/datasources";
