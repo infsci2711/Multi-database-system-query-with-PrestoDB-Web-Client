f6ae147dcceb7b9710f6e0349c0a99b00a823982
ndsorrowchi <ndsorrowchi@gmail.com>
2015-04-11 23:26:21 -0400

    corrected ip of metastore

--- javascripts/knockout_models/global.js
+++ javascripts/knockout_models/global.js
@@ -1,3 +1,4 @@
-var isInDebug = true;
-var restBaseUrl = "http://54.174.80.167:7654/";
-var metaStoreGetDataSourcesURL =  "123456/Datasources";
+var isInDebug = false;
+
+var restBaseUrl = "http://127.0.0.1:7654/";
+var metaStoreGetDataSourcesURL =  "http://54.152.26.131:7654/datasources";
