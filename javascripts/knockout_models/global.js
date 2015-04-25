965004e7d0b92527906b437115569f5cbb6c1807
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-04-25 09:37:05 -0500

    set global js to colfusion values

--- javascripts/knockout_models/global.js
+++ javascripts/knockout_models/global.js
@@ -1,4 +1,4 @@
 var isInDebug = false;
 
-var restBaseUrl = "http://54.174.80.167:7654/";
-var metaStoreGetDataSourcesURL =  "http://54.152.26.131:7654/datasources";
+var restBaseUrl = "http://colfusion.exp.sis.pitt.edu/queryrest/"; //"http://54.174.80.167:7654/";
+var metaStoreGetDataSourcesURL = "http://colfusion.exp.sis.pitt.edu/metastorerest/datasources"; //"http://54.152.26.131:7654/datasources";
